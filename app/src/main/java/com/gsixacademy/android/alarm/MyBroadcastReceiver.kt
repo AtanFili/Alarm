0f6e102a78f0a3b37696b15d4c8a29a825df1401
Home Pc <f_atanasovski@yahoo.com>
2021-05-06 22:03:18 +0200

    Initial commit

---
+++ app/src/main/java/com/gsixacademy/android/alarm/MyBroadcastReceiver.kt
@@ -0,0 +1,19 @@
+package com.gsixacademy.android.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+
+
+class MyBroadcastReceiver :BroadcastReceiver()
+{
+    override fun onReceive(p0: Context?, p1 : Intent?) {
+        var i =Intent(p0,AlarmOnActivity::class.java)
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        p0?.startActivity(i)
+
+    }
+
+}
+
