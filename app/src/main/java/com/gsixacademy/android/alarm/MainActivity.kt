ed2f9e52032b0daef89d7f68fd8904b1deb33876
Home Pc <f_atanasovski@yahoo.com>
2021-05-12 21:39:54 +0200

    second commit

--- app/src/main/java/com/gsixacademy/android/alarm/MainActivity.kt
+++ app/src/main/java/com/gsixacademy/android/alarm/MainActivity.kt
@@ -14,13 +14,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         button.setOnClickListener {
-            var sec=editText.text.toString().toInt()
+
             var i=Intent(applicationContext,MyBroadcastReceiver::class.java)
             var pi=PendingIntent.getBroadcast(applicationContext,111,i,0)
             var am:AlarmManager=getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            am.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis() +(sec*1000),pi)
-            Toast.makeText(applicationContext,"Alarm is set for $sec Seconds",Toast.LENGTH_LONG).show()
+            am.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+(5*1000),pi)
+            Toast.makeText(applicationContext,"Hey!!Soft boiled eggs are now done...bonn appetitt ",Toast.LENGTH_LONG).show()
+
+
+        }
+        button3.setOnClickListener {
+            var i=Intent(applicationContext,MyBroadcastReceiver::class.java)
+            var pi=PendingIntent.getBroadcast(applicationContext,111,i,0)
+            var am:AlarmManager=getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            am.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+(15*1000),pi)
+            Toast.makeText(applicationContext,"Hey!!Medium boiled eggs are now done...bonn appetitt ",Toast.LENGTH_LONG).show()
+
+
+
+
+
+        }
+        button4.setOnClickListener {
+            var i=Intent(applicationContext,MyBroadcastReceiver::class.java)
+            var pi=PendingIntent.getBroadcast(applicationContext,111,i,0)
+            var am:AlarmManager=getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            am.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis()+(20*1000),pi)
+            Toast.makeText(applicationContext,"Hey!!Hard boiled eggs are now done...bonn appetitt ",Toast.LENGTH_LONG).show()
+
+
+
 
 
         }
